9dfdd3a7ae6ec5828615bcdd12d60aa77ffaf425
Charles Edward Pax <charles.pax@gmail.com>
2017-09-12 01:09:45 +0100

    Added doxygen comments to foo.c.

--- foo.c
+++ foo.c
@@ -1,3 +1,33 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Charles Edward Pax                                   *
+ *                                                                            *
+ * This file is a part of Hello                                               *
+ *                                                                            *
+ * Hello is an example program demonstrating several aspects of               *
+ * C programming, Doxygen documentation, and Make.                            *
+ *                                                                            *
+ * Hello is distributed in the hope that it will be useful, but WITHOUT       *
+ * ANY WARRANTY; eithout even the implied warranty of MERCHANTABILITY or      *
+ * FITNESS FOR A PARTIFULAR PURPOSE. See the GNU Lesser General Public        *
+ * License for more details.                                                  *
+ *                                                                            *
+ * You should have received a copy of the GNU Lesser General Public License   *
+ * along with Box. If not, see <http://www.gnu.org/license/>.                 *
+ *                                                                            *
+ ******************************************************************************/
+
+/**
+ * \file foo.c
+ * \author Charles Edward Pax <charles.pax@gmail.com>
+ * \date 2017-07-18
+ * \brief File containing example C code and Doxygen documentation
+ *
+ * \details This is where would be written a more in-depth explaination of
+ * this file and what it does.
+ *
+ * @see http://paxinstruments.com/store/
+ */
+
 #include "foo.h"  /* Include the header (not strictly necessary here) */
 
 /**
