8d446aa401ffa7e17caa704797c3da5f6aea0ce5
Charles Edward Pax <charles.pax@gmail.com>
2018-12-29 08:49:58 -0500

    Added a Python3 example.

---
+++ hello_python/hello.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+print("Hello, world!")
+
+def example(text):
+    print(text)
+    return
+
+def main():
+    example("print this statement.")
+    print("This function name is", __name__)
+
+if __name__ == "__main__":
+    main()
+
+
