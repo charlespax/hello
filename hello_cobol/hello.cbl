7ae5a75a216e80aca4c819fa877676b7796cf5f3
Charles Edward Pax <charles.pax@gmail.com>
2024-05-27 21:03:45 -0400

    Added cobol

---
+++ hello_cobol/hello.cbl
@@ -0,0 +1,10 @@
+      * run cobc -x hello.cbl to compile.
+      * run ./hello to run the compile program.
+       Identification Division.
+       Program-ID. sampleCOBOL.
+
+       Procedure Division.
+       Main-Paragraph.
+           Display "Hello World!"
+           Stop Run.
+
